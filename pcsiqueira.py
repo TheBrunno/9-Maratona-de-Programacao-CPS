b5666167e725d651725ad6099c40cff129050551
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-10-23 01:16:21 -0300

    Delete pcsiqueira.py

--- pcsiqueira.py
+++
@@ -1,5 +0,0 @@
-import random
-
-x = input("Digite o nome das crianças quais você sente atração: ").split()
-pedo = len(x) * random.randint(1, 10)
-print('Você foi sentenciado a', pedo, 'anos na prisão')
